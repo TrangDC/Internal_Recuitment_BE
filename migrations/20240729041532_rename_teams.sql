bc032533c57561cb16b8dd3ef98a49e794eb1af0
tom.tran <tom@ITS-TIENTV.Techvify.Internal>
2024-07-30 15:57:47 +0700

    fix(migration): drop to rename user team_id

--- migrations/20240729041532_rename_teams.sql
+++ migrations/20240729041532_rename_teams.sql
@@ -1,7 +1,7 @@
 -- Delete foreign keys
 ALTER TABLE IF EXISTS team_managers DROP CONSTRAINT IF EXISTS team_managers_team_id_fkey;
 ALTER TABLE IF EXISTS hiring_jobs DROP CONSTRAINT IF EXISTS hiring_jobs_team_id_fkey;
-ALTER TABLE IF EXISTS users DROP COLUMN IF EXISTS team_id;
+ALTER TABLE IF EXISTS users RENAME COLUMN team_id TO hiring_team_id;
 
 -- Rename teams to hiring_teams
 ALTER TABLE IF EXISTS teams RENAME TO hiring_teams;
